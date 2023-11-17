0b18490b0738b6c3f5ee5352362bbc67549515df
David Flores <david.flores@itsoft.mx>
2023-11-17 08:11:23 -0600

    fix: :fire: Fix UserIcon on screens with height below 670 on LoginScreen

--- lib/widgets/auth_background.dart
+++ lib/widgets/auth_background.dart
@@ -78,7 +78,7 @@ class UserIcon extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
-    final distance = size.height - (size.height - 80);
+    final distance = size.height - ( size.height - ((size.height < 670) ? 50 : 80) );
 
     return SafeArea(
       child: Container(
